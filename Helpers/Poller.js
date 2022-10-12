547d215e6b013542cff941154df9d886fb5f2e0c
WiZu <wilcozuidema@gmail.com>
2022-10-12 21:12:20 +0200

    Added Poller script

---
+++ Helpers/Poller.js
@@ -0,0 +1,18 @@
+export const Poll = (callback, timeout, interval) => {
+  let endTime = Number(new Date()) + (timeout || 2000);
+  interval = interval || 100;
+
+  const checkCondition = (resolve, reject) => {
+    const result = fn();
+    if (result) {
+      resolve(result);
+    }
+    else if (Number(new Date()) < endTime) {
+      setTimeout(checkCondition, interval, resolve, reject);
+    }
+    else {
+      reject(new Error(`timed out for ${fn}: ${arguments}`));
+    }
+  };
+  return new Promise(checkCondition);
+};
